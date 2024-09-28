a0bce6cda055600d5478d5c1067d569a17316f57
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-09-28 01:31:25 -0400

    Update items.ts

--- data/mods/teraevolution/items.ts
+++ data/mods/teraevolution/items.ts
@@ -98,4 +98,34 @@ export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
 		gen: 9,
 		desc: "Ogerpon-Teal: Tera Evolve to gain Embody Aspect.",
 	},
+	terashard: {
+		name: "Tera Shard",
+		spritenum: 658,
+		onTakeItem: false,
+		onStart(pokemon) {
+			if ([
+					'Meowscarada', 'Skeledirge', 'Quaquaval', 'Pawmot', 'Mabosstiff', 'Glimmora', 'Ursaluna', 'Sudowoodo', 'Mismagius',
+					'Crabominable', 'Toxtricity', 'Florges', 'Altaria', 'Clodsire', 'Tinkaton', 'Flamigo', 'Hydrapple', 'Dragonite', 'Cinccino',
+					'Grafaiai', 'Serperior', 'Emboar', 'Samurott', 'Cyclizar', 'Breloom', 'Mamoswine', 'Dewgong', 'Malamar', 'Toxicroak',
+					'Kricketune', 'Granbull', 'Spiritomb', 'Clawitzer', 'Dragalge', 'Dusknoir', 'Volcanion', 'Greedent', 'Honchkrow', 'Zoroark',
+					'Armarouge', 'Ceruledge', 'Baxcalibur', 'Koraidon', 'Miraidon', 'Terapagos'
+				].includes(pokemon.baseSpecies.baseSpecies)) {
+	  			if (pokemon.side.sideConditions['teraused']) {
+	  				pokemon.canTerastallize = null;
+	  			} else {
+	        		pokemon.canTerastallize = this.actions.canTerastallize(pokemon);
+	  			}
+      	}
+		},
+		itemUser: [
+			'Meowscarada', 'Skeledirge', 'Quaquaval', 'Pawmot', 'Mabosstiff', 'Glimmora', 'Ursaluna', 'Sudowoodo', 'Mismagius',
+			'Crabominable', 'Toxtricity', 'Florges', 'Altaria', 'Clodsire', 'Tinkaton', 'Flamigo', 'Hydrapple', 'Dragonite', 'Cinccino',
+			'Grafaiai', 'Serperior', 'Emboar', 'Samurott', 'Cyclizar', 'Breloom', 'Mamoswine', 'Dewgong', 'Malamar', 'Toxicroak',
+			'Kricketune', 'Granbull', 'Spiritomb', 'Clawitzer', 'Dragalge', 'Dusknoir', 'Volcanion', 'Greedent', 'Honchkrow', 'Zoroark',
+			'Armarouge', 'Ceruledge', 'Baxcalibur', 'Koraidon', 'Miraidon', 'Terapagos', 'Samurott-Hisui', 'Zoroark-Hisui', 'Toxtricity-Low-Key' 
+		],
+		num: -1001,
+		gen: 9,
+		desc: "Allows certain Pokemon to Tera Evolve.",
+	},
 };
