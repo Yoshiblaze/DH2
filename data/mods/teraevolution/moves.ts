0ef61afd28ce2dc5e9d7f70ea5b23312eea6852a
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-09-28 01:02:44 -0400

    Update moves.ts

--- data/mods/teraevolution/moves.ts
+++ data/mods/teraevolution/moves.ts
@@ -1,4 +1,24 @@
 export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
+	facade: {
+		num: 263,
+		accuracy: 100,
+		basePower: 70,
+		category: "Physical",
+		shortDesc: "Power doubles if user is statused.",
+		name: "Facade",
+		pp: 20,
+		priority: 0,
+		flags: {contact: 1, protect: 1, mirror: 1},
+		onBasePower(basePower, pokemon) {
+			if (pokemon.status || pokemon.hasAbility('comatose')) {
+				return this.chainModify(2);
+			}
+		},
+		secondary: null,
+		target: "normal",
+		type: "Normal",
+		contestType: "Cute",
+	},
   teraused: {
 		shortDesc: "Prevents Terastalization from being used multiple times.",
 		accuracy: true,
