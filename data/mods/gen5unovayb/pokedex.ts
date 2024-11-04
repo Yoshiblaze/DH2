e65b28c65795d0676f050f550c9b093c00d7ef58
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-11-03 23:37:15 -0500

    Create pokedex.ts

---
+++ data/mods/gen5unovayb/pokedex.ts
@@ -0,0 +1,309 @@
+export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable = {
+	snivy: {
+		inherit: true,
+		unreleasedHidden: null,
+	},
+	servine: {
+		inherit: true,
+		unreleasedHidden: null,
+	},
+	serperior: {
+		inherit: true,
+		unreleasedHidden: null,
+	},
+	tepig: {
+		inherit: true,
+		unreleasedHidden: null,
+	},
+	pignite: {
+		inherit: true,
+		unreleasedHidden: null,
+	},
+	emboar: {
+		inherit: true,
+		unreleasedHidden: null,
+	},
+	oshawott: {
+		inherit: true,
+		abilities: {0: "Torrent", H: "Sharpness"},
+		unreleasedHidden: null,
+	},
+	dewott: {
+		inherit: true,
+		abilities: {0: "Torrent", H: "Sharpness"},
+		unreleasedHidden: null,
+	},
+	samurott: {
+		inherit: true,
+		abilities: {0: "Torrent", H: "Sharpness"},
+		unreleasedHidden: null,
+	},
+	watchog: {
+		inherit: true,
+		baseStats: {hp: 60, atk: 105, def: 79, spa: 60, spd: 79, spe: 77},
+	},
+	stoutland: {
+		inherit: true,
+		baseStats: {hp: 85, atk: 110, def: 90, spa: 45, spd: 90, spe: 80},
+	},
+	purrloin: {
+		inherit: true,
+		types: ["Dark", "Normal"],
+	},
+	liepard: {
+		inherit: true,
+		types: ["Dark", "Normal"],
+		baseStats: {hp: 68, atk: 88, def: 50, spa: 88, spd: 50, spe: 116},
+	},
+	pansage: {
+		inherit: true,
+		abilities: {0: "Gluttony", 1: "Leaf Guard", H: "Overgrow"},
+		maleOnlyHidden: null,
+	},
+	simisage: {
+		inherit: true,
+		abilities: {0: "Gluttony", 1: "Leaf Guard", H: "Overgrow"},
+		maleOnlyHidden: null,
+	},
+	pansear: {
+		inherit: true,
+		abilities: {0: "Gluttony", 1: "Solar Power", H: "Blaze"},
+		maleOnlyHidden: null,
+	},
+	simisear: {
+		inherit: true,
+		abilities: {0: "Gluttony", 1: "Solar Power", H: "Blaze"},
+		maleOnlyHidden: null,
+	},
+	panpour: {
+		inherit: true,
+		abilities: {0: "Gluttony", 1: "Rain Dish", H: "Torrent"},
+		maleOnlyHidden: null,
+	},
+	simipour: {
+		inherit: true,
+		abilities: {0: "Gluttony", 1: "Rain Dish", H: "Torrent"},
+		maleOnlyHidden: null,
+	},
+	musharna: {
+		inherit: true,
+		baseStats: {hp: 126, atk: 55, def: 85, spa: 107, spd: 95, spe: 29},
+	},
+	unfezant: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 115, def: 80, spa: 65, spd: 55, spe: 93},
+	},
+	roggenrola: {
+		inherit: true,
+		abilities: {0: "Sturdy", 1: "Weak Armor", H: "Sand Force"},
+	},
+	boldore: {
+		inherit: true,
+		abilities: {0: "Sturdy", 1: "Weak Armor", H: "Sand Force"},
+	},
+	gigalith: {
+		inherit: true,
+		baseStats: {hp: 85, atk: 135, def: 130, spa: 60, spd: 80, spe: 25},
+		abilities: {0: "Sturdy", 1: "Sand Stream", H: "Sand Force"},
+	},
+	woobat: {
+		inherit: true,
+		baseStats: {hp: 65, atk: 45, def: 43, spa: 55, spd: 43, spe: 72},
+	},
+	swoobat: {
+		inherit: true,
+		baseStats: {hp: 77, atk: 57, def: 55, spa: 77, spd: 55, spe: 114},
+	},
+	drilbur: {
+		inherit: true,
+		abilities: {0: "Sand Force", H: "Mold Breaker"},
+	},
+	excadrill: {
+		inherit: true,
+		abilities: {0: "Sand Force", H: "Mold Breaker"},
+	},
+	seismitoad: {
+		inherit: true,
+		baseStats: {hp: 105, atk: 89, def: 75, spa: 85, spd: 75, spe: 74},
+	},
+	throh: {
+		inherit: true,
+		baseStats: {hp: 135, atk: 100, def: 85, spa: 30, spd: 85, spe: 45},
+	},
+	sawk: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 135, def: 75, spa: 30, spd: 75, spe: 85},
+	},
+	leavanny: {
+		inherit: true,
+		baseStats: {hp: 75, atk: 103, def: 80, spa: 70, spd: 80, spe: 92},
+	},
+	venipede: {
+		inherit: true,
+		abilities: {0: "Poison Point", 1: "Swarm", H: "Speed Boost"},
+	},
+	whirlipede: {
+		inherit: true,
+		abilities: {0: "Poison Point", 1: "Swarm", H: "Speed Boost"},
+	},
+	scolipede: {
+		inherit: true,
+		baseStats: {hp: 60, atk: 100, def: 89, spa: 55, spd: 69, spe: 112},
+		abilities: {0: "Poison Point", 1: "Swarm", H: "Speed Boost"},
+	},
+	basculin: {
+		inherit: true,
+		abilities: {0: "Reckless", 1: "Adaptability", H: "Drizzle"},
+	},
+	basculinbluestriped: {
+		inherit: true,
+		abilities: {0: "Reckless", 1: "Adaptability", H: "Drizzle", S: "Rock Head"},
+	},
+	krookodile: {
+		inherit: true,
+		baseStats: {hp: 95, atk: 117, def: 80, spa: 65, spd: 70, spe: 92},
+	},
+	maractus: {
+		inherit: true,
+		baseStats: {hp: 75, atk: 60, def: 93, spa: 106, spd: 67, spe: 74},
+		abilities: {0: "Sand Veil", 1: "Chlorophyll", H: "Storm Drain"},
+	},
+	garbodor: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 95, def: 92, spa: 60, spd: 92, spe: 75},
+	},
+	zorua: {
+		inherit: true,
+		types: ["Dark", "Ghost"],
+		abilities: {0: "Illusion", H: "Imposter"},
+	},
+	zoroark: {
+		inherit: true,
+		types: ["Dark", "Ghost"],
+		abilities: {0: "Illusion", H: "Imposter"},
+	},
+	gothita: {
+		inherit: true,
+		types: ["Psychic", "Dark"],
+		abilities: {0: "Frisk", 1: "Anticipation", H: "Shadow Tag"},
+		unreleasedHidden: null,
+	},
+	gothorita: {
+		inherit: true,
+		types: ["Psychic", "Dark"],
+		abilities: {0: "Frisk", 1: "Anticipation", H: "Shadow Tag"},
+		maleOnlyHidden: null,
+	},
+	gothitelle: {
+		inherit: true,
+		types: ["Psychic", "Dark"],
+		abilities: {0: "Frisk", 1: "Intimidate", H: "Shadow Tag"},
+		maleOnlyHidden: null,
+	},
+	ducklett: {
+		inherit: true,
+		abilities: {0: "Gale Wings", 1: "Big Pecks", H: "Hydration"},
+	},
+	swanna: {
+		inherit: true,
+		abilities: {0: "Gale Wings", 1: "Big Pecks", H: "Hydration"},
+	},
+	vanillite: {
+		inherit: true,
+		abilities: {0: "Ice Body", 1: "Slush Rush", H: "Weak Armor"},
+	},
+	vanillish: {
+		inherit: true,
+		abilities: {0: "Ice Body", 1: "Slush Rush", H: "Weak Armor"},
+	},
+	vanilluxe: {
+		inherit: true,
+		abilities: {0: "Ice Body", 1: "Snow Warning", H: "Weak Armor"},
+	},
+	emolga: {
+		inherit: true,
+		baseStats: {hp: 55, atk: 45, def: 70, spa: 55, spd: 77, spe: 103},
+	},
+	foongus: {
+		inherit: true,
+		abilities: {0: "Natural Cure", H: "Regenerator"},
+	},
+	amoonguss: {
+		inherit: true,
+		abilities: {0: "Natural Cure", H: "Regenerator"},
+	},
+	galvantula: {
+		inherit: true,
+		baseStats: {hp: 70, atk: 77, def: 68, spa: 107, spd: 60, spe: 108},
+	},
+	ferroseed: {
+		inherit: true,
+		abilities: {0: "Iron Barbs", H: "Anticipation"},
+	},
+	ferrothorn: {
+		inherit: true,
+		abilities: {0: "Iron Barbs", H: "Anticipation"},
+	},
+	klink: {
+		inherit: true,
+		unreleasedHidden: null,
+	},
+	klang: {
+		inherit: true,
+		baseStats: {hp: 70, atk: 80, def: 95, spa: 60, spd: 85, spe: 50},
+	},
+	klinklang: {
+		inherit: true,
+		baseStats: {hp: 75, atk: 100, def: 115, spa: 60, spd: 85, spe: 90},
+	},
+	eelektross: {
+		inherit: true,
+		baseStats: {hp: 90, atk: 115, def: 80, spa: 105, spd: 80, spe: 50},
+	},
+	beheeyem: {
+		inherit: true,
+		baseStats: {hp: 75, atk: 75, def: 75, spa: 140, spd: 95, spe: 40},
+	},
+	cubchoo: {
+		inherit: true,
+		abilities: {0: "Snow Cloak", 1: "Slush Rush", H: "Rattled"},
+	},
+	beartic: {
+		inherit: true,
+		baseStats: {hp: 95, atk: 130, def: 80, spa: 70, spd: 80, spe: 50},
+		abilities: {0: "Snow Cloak", 1: "Slush Rush", H: "Rattled"},
+	},
+	bouffalant: {
+		inherit: true,
+		baseStats: {hp: 95, atk: 110, def: 65, spa: 65, spd: 95, spe: 55},
+		abilities: {0: "Reckless", 1: "Soundproof", H: "Fur Coat"},
+	},
+	litwick: {
+		inherit: true,
+		unreleasedHidden: null,
+	},
+	lampent: {
+		inherit: true,
+		unreleasedHidden: null,
+	},
+	chandelure: {
+		inherit: true,
+		unreleasedHidden: null,
+	},
+	rufflet: {
+		inherit: true,
+		unreleasedHidden: null,
+	},
+	heatmor: {
+		inherit: true,
+		abilities: {0: "Drought", 1: "Flash Fire", H: "White Smoke"},
+	},
+	larvesta: {
+		inherit: true,
+		unreleasedHidden: null,
+	},
+	volcarona: {
+		inherit: true,
+		unreleasedHidden: null,
+	},
+};
