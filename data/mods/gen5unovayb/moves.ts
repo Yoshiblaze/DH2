0cc721b921458d9c4e94059668f100e945720752
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-11-04 14:09:10 -0500

    Create moves.ts

---
+++ data/mods/gen5unovayb/moves.ts
@@ -0,0 +1,151 @@
+export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
+	hail: {
+		num: 258,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		shortDesc: "For 5 turns, snow falls. Ice: 1.5x Def.",
+		isNonstandard: "Past",
+		name: "Hail",
+		pp: 10,
+		priority: 0,
+		flags: {metronome: 1},
+		weather: 'snow',
+		secondary: null,
+		target: "all",
+		type: "Ice",
+		zMove: {boost: {spe: 1}},
+		contestType: "Beautiful",
+	},
+	facade: {
+		num: 263,
+		accuracy: 100,
+		basePower: 70,
+		category: "Physical",
+		shortDesc: "Power doubles if user is statused.",
+		name: "Facade",
+		pp: 20,
+		priority: 0,
+		flags: {contact: 1, protect: 1, mirror: 1},
+		onBasePower(basePower, pokemon) {
+			if (pokemon.status || pokemon.hasAbility('comatose')) {
+				return this.chainModify(2);
+			}
+		},
+		secondary: null,
+		target: "normal",
+		type: "Normal",
+		contestType: "Cute",
+	},
+	defog: {
+		num: 432,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		shortDesc: "-1 evasion; clears hazards on both sides.",
+		name: "Defog",
+		pp: 15,
+		priority: 0,
+		flags: {protect: 1, reflectable: 1, mirror: 1, bypasssub: 1, metronome: 1, wind: 1},
+		onHit(target, source, move) {
+			let success = false;
+			if (!target.volatiles['substitute'] || move.infiltrates) success = !!this.boost({evasion: -1});
+			const removeTarget = [
+				'reflect', 'lightscreen', 'auroraveil', 'safeguard', 'mist', 'spikes', 'toxicspikes', 'stealthrock', 'stickyweb', 'gmaxsteelsurge',
+			];
+			const removeAll = [
+				'spikes', 'toxicspikes', 'stealthrock', 'stickyweb', 'gmaxsteelsurge',
+			];
+			for (const targetCondition of removeTarget) {
+				if (target.side.removeSideCondition(targetCondition)) {
+					if (!removeAll.includes(targetCondition)) continue;
+					this.add('-sideend', target.side, this.dex.conditions.get(targetCondition).name, '[from] move: Defog', '[of] ' + source);
+					success = true;
+				}
+			}
+			for (const sideCondition of removeAll) {
+				if (source.side.removeSideCondition(sideCondition)) {
+					this.add('-sideend', source.side, this.dex.conditions.get(sideCondition).name, '[from] move: Defog', '[of] ' + source);
+					success = true;
+				}
+			}
+			this.field.clearTerrain();
+			return success;
+		},
+		secondary: null,
+		target: "normal",
+		type: "Flying",
+		zMove: {boost: {accuracy: 1}},
+		contestType: "Cool",
+	},
+	incinerate: {
+		inherit: true,
+		basePower: 60,
+	},
+	batonpass: {
+		num: 226,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		shortDesc: "User switches out.",
+		name: "Baton Pass",
+		pp: 40,
+		priority: 0,
+		flags: {metronome: 1},
+		onHit(target) {
+			if (!this.canSwitch(target.side) || target.volatiles['commanded']) {
+				this.attrLastMove('[still]');
+				this.add('-fail', target);
+				return this.NOT_FAIL;
+			}
+		},
+		selfSwitch: true,
+		secondary: null,
+		target: "self",
+		type: "Normal",
+		zMove: {effect: 'clearnegativeboost'},
+		contestType: "Cute",
+	},
+	stormthrow: {
+		inherit: true,
+		basePower: 50,
+	},
+	frostbreath: {
+		inherit: true,
+		basePower: 50,
+		accuracy: 85,
+	},
+	tidyup: {
+		inherit: true,
+    isNonstandard: null,
+    gen: 5,
+	},
+	relicsong: {
+		num: 547,
+		accuracy: 100,
+		basePower: 50,
+		category: "Special",
+		name: "Relic Song",
+		pp: 20,
+		priority: 1,
+		flags: {protect: 1, mirror: 1, sound: 1, bypasssub: 1},
+		secondary: null,
+		onModifyMove(move, pokemon) {
+			if (pokemon.getStat('atk', false, true) > pokemon.getStat('spa', false, true)) move.category = 'Physical';
+		},
+		onHit(target, pokemon, move) {
+			if (pokemon.baseSpecies.baseSpecies === 'Meloetta' && !pokemon.transformed) {
+				move.willChangeForme = true;
+			}
+		},
+		onAfterMoveSecondarySelf(pokemon, target, move) {
+			if (move.willChangeForme) {
+				const meloettaForme = pokemon.species.id === 'meloettapirouette' ? '' : '-Pirouette';
+				pokemon.formeChange('Meloetta' + meloettaForme, this.effect, false, '[msg]');
+			}
+		},
+		target: "allAdjacentFoes",
+		type: "Normal",
+		contestType: "Beautiful",
+	},
+};
