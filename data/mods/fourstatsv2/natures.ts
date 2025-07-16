0e48a2b04ff8a1fc83874734d6cf6f746ea2a911
Yoshiblaze <rosefias@gmail.com>
2025-07-16 17:35:04 -0400

    four stats v2

---
+++ data/mods/fourstatsv2/natures.ts
@@ -0,0 +1,109 @@
+export const Natures: import('../../../sim/dex-data').ModdedNatureDataTable = {
+	adamant: {
+		name: "Adamant",
+		plus: ['atk', 'def'],
+		minus: ['spa', 'spd'],
+	},
+	bashful: {
+		name: "Bashful",
+	},
+	bold: {
+		name: "Bold",
+	},
+	brave: {
+		name: "Brave",
+		plus: ['atk', 'def'],
+		minus: 'spe',
+	},
+	calm: {
+		name: "Calm",
+		plus: ['spa', 'spd'],
+		minus: ['atk', 'def'],
+	},
+	careful: {
+		name: "Careful",
+	},
+	docile: {
+		name: "Docile",
+	},
+	gentle: {
+		name: "Gentle",
+		plus: ['spa', 'spd'],
+		minus: ['atk', 'def'],
+	},
+	hardy: {
+		name: "Hardy",
+	},
+	hasty: {
+		name: "Hasty",
+		plus: 'spe',
+		minus: ['atk', 'def'],
+	},
+	impish: {
+		name: "Impish",
+		plus: ['atk', 'def'],
+		minus: ['spa', 'spd'],
+	},
+	jolly: {
+		name: "Jolly",
+		plus: 'spe',
+		minus: ['spa', 'spd'],
+	},
+	lax: {
+		name: "Lax",
+		plus: ['atk', 'def'],
+		minus: ['spa', 'spd'],
+	},
+	lonely: {
+		name: "Lonely",
+	},
+	mild: {
+		name: "Mild",
+		plus: ['spa', 'spd'],
+		minus: ['atk', 'def'],
+	},
+	modest: {
+		name: "Modest",
+		plus: ['spa', 'spd'],
+		minus: ['atk', 'def'],
+	},
+	naive: {
+		name: "Naive",
+		plus: 'spe',
+		minus: ['spa', 'spd'],
+	},
+	naughty: {
+		name: "Naughty",
+		plus: ['atk', 'def'],
+		minus: ['spa', 'spd'],
+	},
+	quiet: {
+		name: "Quiet",
+		plus: ['spa', 'spd'],
+		minus: 'spe',
+	},
+	quirky: {
+		name: "Quirky",
+	},
+	rash: {
+		name: "Rash",
+	},
+	relaxed: {
+		name: "Relaxed",
+		plus: ['atk', 'def'],
+		minus: 'spe',
+	},
+	sassy: {
+		name: "Sassy",
+		plus: ['spa', 'spd'],
+		minus: 'spe',
+	},
+	serious: {
+		name: "Serious",
+	},
+	timid: {
+		name: "Timid",
+		plus: 'spe',
+		minus: ['atk', 'def'],
+	},
+};
