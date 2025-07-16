e10a2aac4614e31695ed48e911a4e44938d0e820
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2025-07-15 21:34:25 -0400

    Update rulesets.ts

--- data/mods/fourstats/rulesets.ts
+++ data/mods/fourstats/rulesets.ts
@@ -9,21 +9,5 @@ export const Rulesets: import('../../../sim/dex-formats').ModdedFormatDataTable 
 		onSwap(pokemon) {
 			pokemon.addVolatile('sameboost');
 		},
-		onStart(pokemon) {
-      	const newatk = pokemon.storedStats.def;
-      	const newdef = pokemon.storedStats.atk;
-      	const newspa = pokemon.storedStats.spd;
-      	const newspd = pokemon.storedStats.spa;
-			if (pokemon.getStat('spd', false, true) > pokemon.getStat('spa', false, true)) {
-        		pokemon.storedStats.spa = newspa;
-      	} else if (pokemon.getStat('spa', false, true) > pokemon.getStat('spd', false, true)) {
-        		pokemon.storedStats.spd = newspd;
-      	}
-			if (pokemon.getStat('def', false, true) > pokemon.getStat('atk', false, true)) {
-        		pokemon.storedStats.atk = newatk;
-      	} else if (pokemon.getStat('atk', false, true) > pokemon.getStat('def', false, true)) {
-        		pokemon.storedStats.def = newdef;
-      	}
-		},
 	},
 };
