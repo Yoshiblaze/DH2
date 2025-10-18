4e71ae8d79ff287528df846c3a6039065bdb67f4
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2025-10-18 04:02:37 -0400

    Create moves.ts

---
+++ data/mods/ultraburst/moves.ts
@@ -0,0 +1,10 @@
+export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
+	spectralthief: {
+		inherit: true,
+    isNonstandard: null,
+	},
+	naturesmadness: {
+		inherit: true,
+    isNonstandard: null,
+	},
+};
