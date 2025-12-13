0cb507fec3c502471cd3d05ec06cf042ef7716a8
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2025-12-12 22:49:40 -0500

    Update pokedex.ts

--- data/mods/plza/pokedex.ts
+++ data/mods/plza/pokedex.ts
@@ -552,7 +552,7 @@ export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable 
 		forme: "Mega-X",
 		types: ["Electric"],
 		baseStats: { hp: 60, atk: 135, def: 95, spa: 90, spd: 95, spe: 110 },
-		abilities: { 0: "Static" },
+		abilities: { 0: "Shock Adrenaline" },
 		heightm: 1.2,
 		weightkg: 38,
 		color: "Yellow",
